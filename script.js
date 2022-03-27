ee58d8b7c2dded234fcb1ff748a4ae3a24c2c73b
nisolae <testboy450@gmail.com>
2022-03-27 23:44:55 +0100

    animation and responsiveness update

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
 window.addEventListener('scroll', function(){
-    const header = document.querySelector("header");
+    let header = document.querySelector('header');
     let windowPosition = window.scrollY > 0;
     header.classList.toggle('scrolling-active', windowPosition);
 })
 
-const navSlide = () => {
+ScrollReveal({
+     reset: true,
+     distance :'60px' ,
+     duration: 2500,
+     delay: 400
+});
+
+ScrollReveal().reveal('.heading, .funfact, #home', { delay: 500, origin: 'left' });
+ScrollReveal().reveal('.aboutme, .grid-container, .content', { delay: 600, origin: 'bottom', interval: 200 });
+ScrollReveal().reveal('.uiskill, .getintouch form, #footer', { delay: 500, origin: 'bottom'});
+ScrollReveal().reveal('.devskill', { delay: 500, origin: 'top'});
+ScrollReveal().reveal('.pro img, .socialicons', { delay: 500, origin: 'left', interval: 200});
+ScrollReveal().reveal('.pro .procontent', { delay: 500, origin: 'right', interval: 200});
+ScrollReveal().reveal('.viewwork, .viewmore', { delay: 600, origin: 'bottom'});
+
+
+
+
+
+
+
+
+
+
+
+
+/* const navSlide = () => {
 const burger = document.querySelector('.hamburger');
 const nav = document.querySelector('.nav-list');
 const navLinks = document.querySelectorAll('.nav-list li')
@@ -26,4 +52,4 @@ const navLinks = document.querySelectorAll('.nav-list li')
         burger.classList.toggle('toggle');
     });
  }
-navSlide();
+navSlide(); */
