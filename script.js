16771acf803fff902119f3fde1793e47658cfe0e
Boluwatifee <bjemiriye@gmail.com>
2022-04-05 00:57:34 +0100

    adj 5

--- script.js
+++ script.js
@@ -13,12 +13,20 @@ ScrollReveal({
 
 ScrollReveal().reveal('.heading, .funfact, #home', { delay: 500, origin: 'left' });
 ScrollReveal().reveal('.aboutme, .grid-container, .content', { delay: 600, origin: 'bottom', interval: 200 });
-ScrollReveal().reveal('.uiskill, .getintouch form, #footer', { delay: 500, origin: 'bottom'});
+ScrollReveal().reveal('.uiskill, .getintouch form', { delay: 500, origin: 'bottom'});
 ScrollReveal().reveal('.devskill', { delay: 500, origin: 'top'});
 ScrollReveal().reveal('.pro img, .socialicons', { delay: 500, origin: 'left', interval: 200});
 ScrollReveal().reveal('.pro .procontent', { delay: 500, origin: 'right', interval: 200});
 ScrollReveal().reveal('.viewwork, .viewmore', { delay: 600, origin: 'bottom'});
 
+const hamburger = document.getElementsByClassName('hamburger');
+hamburger.addEventListener('click', responsiveNav);
+
+function responsiveNav(){
+    var x = document.getElementsByClassName('nav-list');
+    
+    x.style.display = 'block';
+}
 
 
 
